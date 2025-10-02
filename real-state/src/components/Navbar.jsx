eaa11fe39babec514866b812b8688bf94a1145ca
brauliomac <brauliomachado100@gmail.com>
2025-10-02 23:52:26 +0100

    mobile menu function close menu added

--- real-state/src/components/Navbar.jsx
+++ real-state/src/components/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from "../assets/assets"
+import { useEffect } from 'react'
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  useEffect(() => {
+    if(showMobileMenu) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  },[showMobileMenu])
+
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -27,13 +41,13 @@ const Navbar = () => {
         <button className=' hidden md:block bg-white px-8 py-2 rounded-full'>
             Sign up
         </button>
-        <img src={ assets.menu_icon } className='md:hidden w-7 cursor-pointer' alt="" />
+        <img onClick={() => setShowMobileMenu(true)} src={ assets.menu_icon } className='md:hidden w-7 cursor-pointer' alt="" />
       </div>
 
       {/*-------- mobile menu------ */}
-      <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all'>
+      <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0'}  right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
         <div className='flex justify-end p-6 cursor-pointer'>
-          <img src={ assets.cross_icon } className='w-6' alt="" />
+          <img onClick={() => setShowMobileMenu(false)} src={ assets.cross_icon } className='w-6' alt="" />
         </div>
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
           <a href="#Header" className='px-4 py-2 rounded-full inline-block' >Home</a>
