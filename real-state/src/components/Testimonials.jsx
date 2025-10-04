5f31647863be140f4ac1fffd16628306dfca2e52
brauliomac <brauliomachado100@gmail.com>
2025-10-04 19:54:24 +0100

    contact form done

--- real-state/src/components/Testimonials.jsx
+++ real-state/src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ const Testimonials = () => {
     <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>
         Customer 
-        <span className='underline underline-offset-4 decoration-1 under font-light'>
+        <span className='underline underline-offset-4 decoration-1 under font-light pl-3'>
             Testimonials
         </span>
       </h1>
