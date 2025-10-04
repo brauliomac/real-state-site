09fe1eef63476521a2f89941fc69a18c70933cc0
brauliomac <brauliomachado100@gmail.com>
2025-10-04 20:36:20 +0100

    footer component created

---
+++ real-state/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <img src={ assets.logo_dark } alt="" />
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quis voluptatibus sequi quod voluptates eius sint 
+                reprehenderit excepturi earum laborum iure eaque 
+                necessitatibus repudiandae ut quia, fugiat, suscipit at. 
+                Quos, atque.
+            </p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
