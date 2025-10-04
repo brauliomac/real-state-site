0bddf8cf163fd315081c09a5c6b2f77a11754133
brauliomac <brauliomachado100@gmail.com>
2025-10-04 18:41:19 +0100

    component Testimonials created

---
+++ real-state/src/components/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
+      <h1>Customer <span></span></h1>
+      <p>Real Stories From Those Who Found Home With Us</p>
+    </div>
+  )
+}
+
+export default Testimonials
