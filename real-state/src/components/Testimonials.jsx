d932073d28489ce713f30b52fedf89a33ca42660
brauliomac <brauliomachado100@gmail.com>
2025-10-04 19:07:05 +0100

    testimonials done

--- real-state/src/components/Testimonials.jsx
+++ real-state/src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { assets, testimonialsData  } from '../assets/assets'
 
 const Testimonials = () => {
   return (
@@ -12,6 +12,29 @@ const Testimonials = () => {
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>
             Real Stories From Those Who Found Home With Us
       </p>
+
+      <div className='flex flex-wrap justify-center gap-8'>
+            { testimonialsData.map((testimonial, index) => (
+                <div key={ index } className='max-w-[340px] border shadow-lg rounded px-8 py-12 text-center'>
+                    <img 
+                        className='w-20 h-20 rounded-full mx-auto mb-4' 
+                        src={ testimonial.image } 
+                        alt={ testimonial.alt } 
+                    />
+                    <h2 className='text-xl text-gray-700 font-medium'>
+                        { testimonial.name }
+                    </h2>
+                    <p className='text-gray-500 mb-4 text-sm'> { testimonial.title }</p>
+
+                    <div className='flex justify-center gap-1 text-red-500 mb-4'>
+                        { Array.from({ length: testimonial.rating }, (item, index) => (
+                            <img src={ assets.star_icon } alt="" key={ index } />
+                        )) }
+                    </div>
+                    <p className='text-gray-600'>{ testimonial.text }</p>
+                </div>
+            )) }
+      </div>
     </div>
   )
 }
