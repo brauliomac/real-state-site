98c75cbf3dd8e246f2a773e591165bd4f5648315
brauliomac <brauliomachado100@gmail.com>
2025-10-04 13:56:41 +0100

    projects useState added functions

--- real-state/src/components/Projects.jsx
+++ real-state/src/components/Projects.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets, projectsData } from '../assets/assets'
 
 const Projects = () => {
+
+    const [ currentIndex, setCurrentIndex] = useState(0)
+    const [carsToShow, setCarsToShow] = useState(1)
+
+    const nextProject = () => {
+        setCurrentIndex ((prevIndex) => (prevIndex + 1) % projectsData.length)
+    }
+
+     const prevProject = () => {
+        setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length -1 : prevIndex -1 )
+    }
+
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>
@@ -29,10 +41,20 @@ const Projects = () => {
         <div className='overflow-hidden'>
             <div className='flex gap-8 transition-transform duration-500 ease-in-out'>
                 { projectsData.map((project, index) => (
-                    <div key={ index }>
-                        <img src={ project.image} alt={ project.title } />
+                    <div key={ index } className='relative flex-shrink-0 w-full sm:w-1/4'>
+                        <img src={ project.image } alt={ project.title } className='w-full h-auto mb-14' />
+                        <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
+                            <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'>
+                                <h2 className='text-xl font-semibold text-gray-800'>
+                                    { project.title }
+                                </h2>
+                                <p className='text-gray-500 text-sm'>
+                                    { project.price } <span> | </span> { project.location }
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                ))}
+                )) }
             </div>
         </div>
     </div>
